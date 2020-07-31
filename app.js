5b5533eceb9f8bdb446c47e24a36b93bf7a71ffc
Rakesh Gupta <rakesh.gupta.chotu016@gmail.com>
2020-07-31 22:23:46 +0530

    bcrypt hash ecryption

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-var encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+
 
 const app = express();
+
+
+const saltRounds = 10;
 
 app.set('view engine', 'ejs');
 
@@ -16,13 +20,11 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/userDB",{ useNewUrlParser: true , useUnifiedTopology: true });
 
-const userSchema=new mongoose.Schema({
+const userSchema={
   email: String,
   password: String
-});
+};
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields: ["password"] });
 
 const User= new mongoose.model("User",userSchema);
 
@@ -38,18 +40,20 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register",function(req,res){
-  const  user=new  User({
-    email:req.body.username,
-    password:req.body.password
-  })
-  user.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  })
-})
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const  user=new  User({
+      email:req.body.username,
+      password:hash
+      })
+      user.save(function(err){
+        if(err){
+          console.log(err);
+        }else{
+          res.render("secrets");
+        }
+      });
+    });
+});
 
 app.post("/login",function(req,res){
   const username=req.body.username;
@@ -59,16 +63,19 @@ app.post("/login",function(req,res){
       console.log(err);
     }
     else{
-      if(foundUser.password===password){
-        res.render("secrets");
-      }
-      else{
-        console.log("Incorrect Password");
-      }
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if(result===true){
+          res.render("secrets");
+        }
+        else{
+          console.log("Incorrect Password");
+        }
+    // result == true
+      });
     }
-  })
+  });
 
-})
+});
 
 
 
