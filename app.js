f4366b49c75951b1b724ed87a72746979935e8ce
Rakesh Gupta <rakesh.gupta.chotu016@gmail.com>
2020-08-13 09:43:19 +0530

    add ssh key

--- app.js
+++ app.js
@@ -5,28 +5,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const bcrypt = require('bcrypt');
-
+const session = require("express-session");
+const passport=require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-
 const saltRounds = 10;
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "technology is my best part",
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB",{ useNewUrlParser: true , useUnifiedTopology: true });
 
-const userSchema={
+const userSchema=new mongoose.Schema({
   email: String,
   password: String
-};
+});
 
+userSchema.plugin(passportLocalMongoose);
 
 const User= new mongoose.model("User",userSchema);
+
+
 
 app.get("/",function(req,res){
   res.render("home");
